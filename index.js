96b6fd4df3c5512385f258bd94f60376833d2d9d
Ilmari <eoksanen@gmail.com>
2020-04-10 14:42:04 +0300

    3.1 with express

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const http = require('http')
-const url = require('url')
+
+const express = require('express')
+const app = express()
+
 
 
 let persons = [
@@ -25,23 +27,19 @@ let persons = [
     }
   ]
 
-const handleReq = (req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' })
+  app.get('/', (req, res) => {
+    res.send('<a href="/api/persons">try this</a>')
+  })
 
-    let path = url.parse(req.url).pathname
+  app.get('/api/persons', (req, res) => {
+    res.json(persons)
+  })
+    
 
-    if(path === '/api/persons') {
-        res.end(JSON.stringify(persons))
-    }
-    else {
-        res.writeHead(404)
-        res.write('please try path /api/persons', res)
-        res.end()
-    }
-}
-
-const app = http.createServer(handleReq)    
 
 const port = 3001
-app.listen(port)
-console.log(`Server running on port ${port}`)
+app.listen(port, () => {
+console.log(`server running on port ${port}`)
+
+})
+
